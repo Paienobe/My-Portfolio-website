1ca18893ca85d8a8febcf8904ae46eb805b57f0c
Progress Aienobe <paienobe@gmail.com>
2022-03-19 19:57:37 +0100

    added new project data

--- src/data.js
+++ src/data.js
@@ -1,9 +1,10 @@
 // projects
-import project1 from './assets/countries-app.png'
-import project2 from './assets/jobs-app.png'
-import project3 from './assets/Comments-app.png'
-import project4 from './assets/space-tourism.png'
-import project5 from './assets/r-p-s.png'
+import project1 from './assets/cryptostatz.png'
+import project2 from './assets/countries-app.png'
+import project3 from './assets/jobs-app.png'
+import project4 from './assets/Comments-app.png'
+import project5 from './assets/space-tourism.png'
+import project6 from './assets/r-p-s.png'
 
 // skills
 import htmlLogo from './assets/icons8-html-5 (1).svg'
@@ -15,13 +16,21 @@ import vsCodeLogo from './assets/icons8-visual-studio-code-2019.svg'
 import gitHubLogo from './assets/icons8-github.svg'
 import tailWindLogo from './assets/tailwindcss-icon.svg'
 
-export const projects = [project1, project2, project3, project4, project5]
+export const projects = [project1, project2, project3, project4, project5, project6]
 export const projectDetails = [
+  {
+    name: 'CryptoStats',
+    description:
+      'This is an app for getting the current price of cryptocurrencies and the entire crypto market. Users can search for crypto currencies and view charted data of these currencies.',
+    image: project2,
+    link: 'https://rest-countries-paienobe.netlify.app/',
+    code: 'https://github.com/Paienobe/REST-Countries-API-with-color-theme-switcher',
+  },
   {
     name: 'Countries App',
     description:
       'This is an app that displays information about all countries in the world. You can search for a specific country or filter countries by region.',
-    image: project1,
+    image: project2,
     link: 'https://rest-countries-paienobe.netlify.app/',
     code: 'https://github.com/Paienobe/REST-Countries-API-with-color-theme-switcher',
   },
@@ -29,7 +38,7 @@ export const projectDetails = [
     name: 'Jobs Filter App',
     description:
       'This is an app that displays a certain amount of jobs. The jobs can be filtered by passing in some requirements.',
-    image: project2,
+    image: project3,
     link: 'https://static-job-listings-app.netlify.app/',
     code: 'https://github.com/Paienobe/Static-Job-Listings-App',
   },
@@ -37,7 +46,7 @@ export const projectDetails = [
     name: 'Comments App',
     description:
       'This is an interactive comments section app. Users can add comments, delete comments, reply to comments, edit comments and replies. I also made use of local storage to preserve the comments.',
-    image: project3,
+    image: project4,
     link: 'https://interactive-comments-app-paienobe.netlify.app/',
     code: 'https://github.com/Paienobe/Interactive-Comments-App',
   },
@@ -45,7 +54,7 @@ export const projectDetails = [
     name: 'Space Tourism Site',
     description:
       'This is a multi-page website built with React. It displays information about destinations for a space misssion. I also shows info about the crew and technologies used for the mission.',
-    image: project4,
+    image: project5,
     link: 'https://space-tourism-website-paienobe.netlify.app/',
     code: 'https://github.com/Paienobe/Space-Tourism-Website',
   },
@@ -53,7 +62,7 @@ export const projectDetails = [
     name: 'Rock, Paper, Scissors',
     description:
       'This is a simple rock, paper, scissors game. Users select an action and the computer picks another to decide a winner',
-    image: project5,
+    image: project6,
     link: 'https://rock-paper-scissors-app-paienobe.netlify.app/',
     code: 'https://github.com/Paienobe/Space-Tourism-Website',
   },
